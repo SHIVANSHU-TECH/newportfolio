a973d1be006a42581cda9f357fe872ffd542c86c
SHIVANSHU-TECH <shivanshushuklacse@delhitechnicalcampus.ac.in>
2026-08-21 00:54:42 +0530

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-import CreatorApp from "@/creator/CreatorApp";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import PositioningStatement from "@/components/PositioningStatement";
+import WhatIBuild from "@/components/WhatIBuild";
+import SelectedWork from "@/components/SelectedWork";
+import MoreWork from "@/components/MoreWork";
+import Process from "@/components/Process";
+import Technology from "@/components/Technology";
+import About from "@/components/About";
+import Availability from "@/components/Availability";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-  return <CreatorApp />;
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <PositioningStatement />
+        <WhatIBuild />
+        <SelectedWork />
+        <MoreWork />
+        <Process />
+        <Technology />
+        <About />
+        <Availability />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
 }
--- src/app/shivanshu/page.tsx
+++ src/app/shivanshu/page.tsx
@@ -1,33 +1,5 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import PositioningStatement from "@/components/PositioningStatement";
-import WhatIBuild from "@/components/WhatIBuild";
-import SelectedWork from "@/components/SelectedWork";
-import MoreWork from "@/components/MoreWork";
-import Process from "@/components/Process";
-import Technology from "@/components/Technology";
-import About from "@/components/About";
-import Availability from "@/components/Availability";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import { redirect } from "next/navigation";
 
 export default function ShivanshuPage() {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <Hero />
-        <PositioningStatement />
-        <WhatIBuild />
-        <SelectedWork />
-        <MoreWork />
-        <Process />
-        <Technology />
-        <About />
-        <Availability />
-        <Contact />
-      </main>
-      <Footer />
-    </>
-  );
+  redirect("/");
 }
